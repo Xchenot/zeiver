d24a2d2d83d4febbd17c2003d44025d82a67844b
Xavie <chenot.xaviier@gmail.com>
2020-11-11 11:10:35 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # zeiver
+
+This project is about the zeiver chatbot.
+
+## Prerequisites
+
+The only tools you need for contribute are:
+
+- Connection to the Webserver
+- A text editor tool, Visual studio code (recommanded)
+- A fttp client, FileZilla (recommanded)
+
+## Install
+
+To install the project, just clone it:
+
+``` bash
+$ git clone git@github.com:Xchenot/zeiver.git
+```
+
+install google cloud sdk 
+https://cloud.google.com/sdk/docs/install#linux
+
+
+## Testing
+
+curl -X POST \
+-H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
+-H "Content-Type: application/json; charset=utf-8" \
+-d @request.json \
+https://dialogflow.googleapis.com/v3beta1/projects/zeiver/locations/us/agents/c7cb8806-ce62-4b90-94dc-9a5f3bbcc8fe/sessions/qnq5t:detectIntent
+
+## Deploying
+
+## Destroying
+
+## Unlocking
+
+## Bash History
+
+## Troubleshooting
---
+++ api-testing/dialogflow.js
@@ -0,0 +1,50 @@
+/**
+ * TODO(developer): Uncomment these variables before running the sample.
+ */
+const projectId = 'zeiver';
+const location = 'global';
+const agentId = 'c7cb8806-ce62-4b90-94dc-9a5f3bbcc8fe';
+const query = ['Hello'];
+const languageCode = 'en'
+
+// Imports the Google Cloud Some API library
+const {SessionsClient} = require('@google-cloud/dialogflow-cx');
+const client = new SessionsClient();
+
+async function detectIntentText() {
+  const sessionId = Math.random().toString(36).substring(7);
+  const sessionPath = client.projectLocationAgentSessionPath(
+    projectId,
+    location,
+    agentId,
+    sessionId
+  );
+  console.info(sessionPath);
+
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: query,
+      },
+      languageCode,
+    },
+  };
+  const [response] = await client.detectIntent(request);
+  console.log(`User Query: ${query}`);
+  for (const message of response.queryResult.responseMessages) {
+    if (message.text) {
+      console.log(`Agent Response: ${message.text.text}`);
+    }
+  }
+  if (response.queryResult.match.intent) {
+    console.log(
+      `Matched Intent: ${response.queryResult.match.intent.displayName}`
+    );
+  }
+  console.log(
+    `Current Page: ${response.queryResult.currentPage.displayName}`
+  );
+}
+
+detectIntentText();
